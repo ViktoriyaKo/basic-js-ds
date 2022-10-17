e1a85d97b6c8df985fcc8cf684e3e35cac70cc56
ViktoriyaKo <kozlova_viktoriy@mail.ru>
2022-10-17 15:57:31 +0300

    feat: change binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -16,27 +16,29 @@ class Node {
 }
 class BinarySearchTree {
   constructor() {
-    this.root = null;
+    this.tree = null;
   }
 
   root() {
-    return this.root;
+    return this.tree;
   }
 
   add(value) {
-    this.root = addWithin(this.root, value);
+    this.tree = addWithin(this.tree, value);
 
     function addWithin(node, value) {
       if (!node) {
+        //нет узла, добавляем. затем он будет потомком для существующего узла
         return new Node(value);
       }
 
       if (node.value === value) {
+        //узлы совпадают, ничего не делаем
         return node;
       }
 
       if (value < node.value) {
-        node.left = addWithin(node.left, value);
+        node.left = addWithin(node.left, value); //рекурсия, кладем или новый узел, или ничего влево
       } else {
         node.right = addWithin(node.right, value);
       }
@@ -45,8 +47,24 @@ class BinarySearchTree {
     }
   }
 
+  find(data) {
+    function findNode(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (node.data < data) {
+        return findNode(node.right, data);
+      }
+      return findNode(node.left, data);
+    }
+    return findNode(this.tree, data);
+  }
+
   has(value) {
-    return searchWithin(this.root, value);
+    return searchWithin(this.tree, value);
 
     function searchWithin(node, value) {
       if (!node) {
@@ -58,32 +76,13 @@ class BinarySearchTree {
       }
 
       return value < node.value
-        ? searchWithin(node.left, value)
+        ? searchWithin(node.left, value) //поиск внутри левого узла (подузла)
         : searchWithin(node.right, value);
     }
   }
 
-  find(data) {
-    function findNode(node, data) {
-      if (!node) {
-        return null;
-      }
-
-      if (node.data === data) {
-        return node;
-      }
-
-      if (data > node.data) {
-        return findNode(node.right, data);
-      } else {
-        return findNode(node.left, data);
-      }
-    }
-
-    return findNode(this.treeRoot, data);
-  }
   remove(value) {
-    this.root = removeNode(this.root, value);
+    this.tree = removeNode(this.tree, value); //удалить узел в поддереве
 
     function removeNode(node, value) {
       if (!node) {
@@ -91,38 +90,42 @@ class BinarySearchTree {
       }
 
       if (value < node.value) {
-        node.left = removeNode(node.left, value);
+        node.left = removeNode(node.left, value); // новое дерево будет в node.left
         return node;
       } else if (node.value < value) {
         node.right = removeNode(node.right, value);
         return node;
       } else {
+        // value === node.value
         // equal - should remove this item
         if (!node.left && !node.right) {
+          // текущий узел лист или нет
           // put null instead of item
           return null;
         }
 
         if (!node.left) {
+          // есть правый потомок
           // set right child instead of item
-          node = node.right;
+          node = node.right; // помещаем правое поддерево
           return node;
         }
 
         if (!node.right) {
+          // есть левый потомок
           // set left child instead of item
           node = node.left;
           return node;
         }
 
         // both children exists for this item
-        let minFromRight = node.right;
+        let minFromRight = node.right; // ищем минимум среди правого
         while (minFromRight.left) {
           minFromRight = minFromRight.left;
         }
         node.value = minFromRight.value;
 
-        node.right = removeNode(node.right, minFromRight.value);
+        node.right = removeNode(node.right, minFromRight.value); // удалить миним узел из правого поддерева
 
         return node;
       }
@@ -130,25 +133,27 @@ class BinarySearchTree {
   }
 
   min() {
-    if (!this.root) {
+    if (!this.tree) {
       return;
     }
 
-    let node = this.root;
+    let node = this.tree;
     while (node.left) {
-      node = node.left;
+      //кто нить меньше слева
+      node = node.left; //самый левый элемент
     }
 
-    return node.value;
+    return node.value; // значение самого маленького
   }
 
   max() {
-    if (!this.root) {
+    if (!this.tree) {
       return;
     }
 
-    let node = this.root;
+    let node = this.tree;
     while (node.right) {
+      //ищем справа
       node = node.right;
     }
 
